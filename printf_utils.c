a3b841853224a650185c6bf69aa40521ffaaa081
Samamtro <fofanasanoussy545@gmail.com>
2025-03-26 11:16:12 +0100

    Update file with function integer and decimal

--- printf_utils.c
+++ printf_utils.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
-/**int print_char(va_list args)
+/**
+ * print_char - function print char
+ * @args: is a variable list
+ * Return: 1
+ */
+int print_char(va_list args)
 {
 	char c = va_arg(args, int);
+
 	_putchar(c);
 	return (1);
 }
 
+/**
+ * print_string - function print string
+ * @args: is a variable list
+ * Return: len
+ */
 int print_string(va_list args)
 {
 	char *s = va_arg(args, char *);
@@ -23,4 +34,47 @@ int print_string(va_list args)
 
 	return (len);
 }
-*/
+
+/**
+ * print_int - function print int
+ * @args: is a variable list
+ * Return: count
+ */
+int print_int(va_list args)
+{
+	int num = va_arg(args, int);
+	int count = 0;
+	char buffer[12];
+	int i = 0, j;
+	unsigned int n;
+
+	if (num < 0)
+	{
+		count += _putchar('-');
+		n = -num;
+	}
+	else
+	{
+		n = num;
+	}
+
+	do {
+		buffer[i++] = (n % 10) + '0';
+		n /= 10;
+	} while (n > 0);
+
+	for (j = i - 1; j >= 0; j--)
+		count += _putchar(buffer[j]);
+
+	return (count);
+}
+
+/**
+ * print_dec - function print dec
+ * @args: is a variable list
+ * Return: print_int(args)
+ */
+int print_dec(va_list args)
+{
+	return (print_int(args));
+}
