5df2ce7413426d0ca501713791f8f80b6624b8a0
IEyZens <roncint61@gmail.com>
2025-03-28 10:51:06 +0100

    Add Commentary

--- _printf.c
+++ _printf.c
@@ -8,44 +8,58 @@
  */
 int _printf(const char *format, ...)
 {
+	/* i permet de parcourir la chaîne de format et printed_chars est un compteur */
 	int i = 0, printed_chars = 0;
 	va_list args;
 
+	/* Vérifie si format est NULL */
 	if (!format)
 		return (-1);
 
 	va_start(args, format);
+	/* Parcours la chaîne de format */
 	while (format[i])
 	{
+		/* Vérifie s'il y a un spécificateur de format */
 		if (format[i] == '%')
 		{
+			/* Vérifie si '%' est le dernier caractère */
 			if (format[i + 1] == '\0')
 			{
 				va_end(args);
 				return (-1);
 			}
 
+			/* Passe au caractère suivant '%' */
 			i++;
+			/* Gestion d'un caractère */
 			if (format[i] == 'c')
 				printed_chars += print_char(args);
+			/* Gestion d'une chaîne de caractères */
 			else if (format[i] == 's')
 				printed_chars += print_string(args);
+			/* Gestion du double '%' */
 			else if (format[i] == '%')
 				printed_chars += _putchar('%');
+			/* Gestion des entiers */
 			else if (format[i] == 'd' || format[i] == 'i')
 				printed_chars += print_int(args);
+			/* Si le spécificateur est inconnu, on affiche le caractère brut */
 			else
 			{
 				printed_chars += _putchar('%');
 				printed_chars += _putchar(format[i]);
 			}
 		}
+		/* Si ce n'est pas un caractère '%', on imprime le caractère */
 		else
 		{
 			printed_chars += _putchar(format[i]);
 		}
 		i++;
 	}
+	/* Termine la gestion des arguments variables */
 	va_end(args);
+	/* Retourne le nombre total d'arguments imprimés */
 	return (printed_chars);
 }
--- printf_utils.c
+++ printf_utils.c
@@ -7,9 +7,12 @@
  */
 int print_char(va_list args)
 {
+	/* Récupère l'argument sous forme de caractère */
 	char c = va_arg(args, int);
 
+	/* Affiche le caractère */
 	_putchar(c);
+	/* Retourne le nombre de caractère imprimés */
 	return (1);
 }
 
@@ -20,18 +23,23 @@ int print_char(va_list args)
  */
 int print_string(va_list args)
 {
+	/* Récupère l'argument sous forme de chaîne */
 	char *s = va_arg(args, char *);
 	int len = 0;
 
+	/* Gère le cas où la chaîne est NULL */
 	if (!s)
+		/* Remplace NULL par "(null)" pour éviter un comportement indéfini */
 		s = "(null)";
 
+	/* Parcours la chaîne et affiche chaque caractère */
 	while (s[len])
 	{
 		_putchar(s[len]);
 		len++;
 	}
 
+	/* Retourne le nombre de caractère imprimés */
 	return (len);
 }
 
@@ -42,29 +50,40 @@ int print_string(va_list args)
  */
 int print_int(va_list args)
 {
+	/* Récupère l'argument sous forme d'entier */
 	int num = va_arg(args, int);
 	int count = 0;
+	/* Tampon pour stocker les chiffres de l'entier (assez grand pour un int) */
 	char buffer[12];
 	int i = 0, j;
 	unsigned int n;
 
+	/* Vérifie si l'entier est négatif */
 	if (num < 0)
 	{
+		/* Affiche le signe moins */
 		count += _putchar('-');
+		/* Convertit en valeur absolue pour la gestion des chiffres */
 		n = -num;
 	}
 	else
 	{
+		/* Garde la valeur telle quelle si elle est positive */
 		n = num;
 	}
 
+	/* Conversion de l'entier en chaîne inversée dans buffer */
 	do {
+		/* Stocke le chiffre sous forme de caractère */
 		buffer[i++] = (n % 10) + '0';
 		n /= 10;
 	} while (n > 0);
 
+	/* Affichage des chiffres dans l'ordre correct */
 	for (j = i - 1; j >= 0; j--)
+		/* Affiche chaque caractère stocké dans buffer */
 		count += _putchar(buffer[j]);
 
+	/* Retourne le nombre de caractères imprimés */
 	return (count);
 }
